db82eabbb2e67c20505ff2e520ab10a5daf272cc
pizzou <pmpumuropizzou@gmail.com>
2024-09-01 18:33:18 +0200

    first commit

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
-import cors from "cors";
+// import cors from "cors"; // Remove the CORS import
 import cookieParser from "cookie-parser";
 import { rateLimit } from "express-rate-limit";
 import { ErrorMiddleware } from "./middleware/error";
@@ -29,13 +29,13 @@ app.use(express.json({ limit: "50mb" }));
 // Cookie parser
 app.use(cookieParser());
 
-// CORS Headers
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+// Remove or comment out CORS headers
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   next();
+// });
 
 // Routes
 app.use(
