80cbc74500d8a3250bd6589787b8bdaaee139e43
pizzou <pmpumuropizzou@gmail.com>
2024-09-01 18:40:38 +0200

    first commit

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
-// import cors from "cors"; // Remove the CORS import
+import cors from "cors"; // Remove the CORS import
 import cookieParser from "cookie-parser";
 import { rateLimit } from "express-rate-limit";
 import { ErrorMiddleware } from "./middleware/error";
@@ -29,15 +29,10 @@ app.use(express.json({ limit: "50mb" }));
 // Cookie parser
 app.use(cookieParser());
 
-// Remove or comment out CORS headers
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   next();
-// });
-
-// Routes
+app.use(cors({
+  origin: 'https://adminhttps-github-com-pizzou-admin-frontend.vercel.app',
+  credentials: true
+}));
 app.use(
   "/api/v1",
   userRouter,
