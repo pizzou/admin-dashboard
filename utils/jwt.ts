cd5eec1d6fc35da6ab7aa8c40f508f0e48205104
pizzou <pmpumuropizzou@gmail.com>
2024-09-01 21:03:32 +0200

    first commit

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -11,43 +11,74 @@ interface ITokenOptions {
   secure?: boolean;
 }
 
-// parse enviroment variables to integrates with fallback values
-const accessTokenExpire = parseInt(
-  process.env.ACCESS_TOKEN_EXPIRE || "300",
-  10
-);
-const refreshTokenExpire = parseInt(
-  process.env.REFRESH_TOKEN_EXPIRE || "1200",
-  10
-);
 
-// options for cookies
-export const accessTokenOptions: ITokenOptions = {
-  expires: new Date(Date.now() + accessTokenExpire * 60 * 60 * 1000),
-  maxAge: accessTokenExpire * 60 * 60 * 1000,
-  httpOnly: true,
-  sameSite: "none",
-  secure: true,
-};
 
-export const refreshTokenOptions: ITokenOptions = {
-  expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
-  maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
-  httpOnly: true,
-  sameSite: "none",
-  secure: true,
-};
+
+
+
+
+
+
+
+
+
+
 
 export const sendToken = (user: IUser, statusCode: number, res: Response) => {
   const accessToken = user.SignAccessToken();
   const refreshToken = user.SignRefreshToken();
-  // upload session to redis
-  redis.set(user._id, JSON.stringify(user) as any);
 
-  res.status(statusCode).json({
-    success: true,
-    user,
-    accessToken,
-    refreshToken,
-  });
+   // upload session to redis
+   redis.set(user._id, JSON.stringify(user) as any);
+
+   res.status(statusCode).json({
+     success: true,
+     user,
+     accessToken,
+     refreshToken,
+   });
+
+   // parse enviroment variables to integrates with fallback values
+
+  const accessTokenExpire = parseInt(
+    process.env.ACCESS_TOKEN_EXPIRE || "300",
+    10
+  );
+
+  const refreshTokenExpire = parseInt(
+    process.env.REFRESH_TOKEN_EXPIRE || "1200",
+    10
+  );
+
+  // options for cookies
+
+  const accessTokenOptions: ITokenOptions = {
+    expires: new Date(Date.now() + accessTokenExpire * 60 * 60 * 1000),
+    maxAge: accessTokenExpire * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "none",
+    secure: true,
+  };
+
+  const refreshTokenOptions: ITokenOptions = {
+    expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
+    maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "none",
+    secure: true,
+  };
+
+  if(process.env.NODE_ENV==="production"){
+    accessTokenOptions.secure= true;
+  }
+
+res.cookie("access_token", accessToken, accessTokenOptions);
+res.cookie("refresh_token", refreshToken, refreshTokenOptions);
+res.status(statusCode).json({
+  success: true,
+  user,
+  accessToken
+})
+
+ 
 };
